ea99946358cc0a3f43cf723a9ad102a2dd1b40ea
Laurynas <laurynas.ant@gmail.com>
2017-03-05 23:40:11 +0200

    testas

--- tests/FileLoaderTest.php
+++ tests/FileLoaderTest.php
@@ -8,9 +8,12 @@ class FileLoaderTest extends PHPUnit_Framework_TestCase
         $f = new FileLoader;
     }
 
-    // Mes norime sukurti musu klasei get() metoda, kuris grazintu failo turini
-    // kataloge __DIR__.'/fixtures/simple.md' pridetas testinis failas
-    // Parasyti papildoma testa, kuris palygintu teskta su failo turiniu.
-    // Palyginimui galime panaudoti this objekto metoda assertEquals
-    // Tekste yra prideta papildoma eilute, todel reiktu itraukti ir newline simboli
+    public function testFileLoaderCanLoadFileContent()
+    {
+        $f = new FileLoader;
+        $r = $f->get(__DIR__.'/fixtures/simple.md');
+        $this->assertEquals("Foo\n", $r);
+    }
+
+    // Parasykime klase siam testui
 }
